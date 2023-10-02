0a31aa31e49551fb591554c54bb1dc92213691d7
Владислав <cymbalov_va@office.osu.ru>
2023-10-02 13:27:45 +0500

    Добавьте файлы проекта.

---
+++ SUBD_CourseWork/Entities/teacher.cs
@@ -0,0 +1,19 @@
+﻿
+namespace SUBD_CourseWork.Entities
+{
+    public class teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BirthDate { get; set; }
+        public string HomeAdress { get; set; }
+        public string Contact { get; set; }
+        public int FacultyCode { get; set; }
+        public int DepartmentCode { get; set; }
+        public string TypeOfCooperation { get; set; }
+        public int DegreeCode { get; set; }
+        public int AcademicTitleCode { get; set; }
+        public int InstitutionCode { get; set; }
+
+    }
+}
---
+++ SUBD_CourseWork/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Query.Expressions.Internal;
+using SUBD_CourseWork;
+using SUBD_CourseWork.Entities;
+
+
+
+namespace SUBD_CourseWork
+{
+    public class ApplicationContext : DbContext
+    {
+ 
+        public DbSet<teacher> teachers { get; set; } = null!;
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=CourseWorkDB;Username=postgres;Password=123;");
+        }
+    }
+    public partial class MainWindow : Window
+    {
+        ApplicationContext db = new ApplicationContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+            var users = db.teachers.ToList();
+            foreach (var user in users)
+            {
+                TestBlock.Text += user.Name + " | " + user.Id + " | " + user.BirthDate + "\n";
+
+            }
+        }
+    }
+}
